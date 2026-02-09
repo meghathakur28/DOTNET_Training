29473e9db55e059cb5a54b3ba041153690cc2a98
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-02-09 23:15:51 +0530

    Add files via upload

---
+++ CollectionPractice/SafeMixedCollectionProcessing/SafeMixedCollectionProcessing/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+namespace SafeMixedCollectionProcessing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // store element of different datatype
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+            arrayList.Add(2);
+            arrayList.Add(20);
+            arrayList.Add(2.34);
+            arrayList.Add("meghathakur");
+
+            //extract only integer values
+            List<int> list = new List<int>();
+            foreach(var i in  arrayList)
+            {
+                int ints;
+                if(Int32.TryParse(i.ToString(),out ints))
+                {
+                    list.Add(ints*ints);
+                }
+            }
+
+            int sum = 0;
+            foreach(var i in  list)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine($"The sum of the integers : {sum}");
+
+
+
+
+        }
+    }
+}
