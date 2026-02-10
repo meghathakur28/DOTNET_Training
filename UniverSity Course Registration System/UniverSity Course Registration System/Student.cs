dafc7d3a973ba248faa261e31ddbeea70f92c28c
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-02-10 16:13:43 +0530

    Add files via upload

---
+++ UniverSity Course Registration System/UniverSity Course Registration System/Student.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University_Course_Registration_System
+{
+    // =========================
+    // Student Class
+    // =========================
+    public class Student
+    {
+        public string StudentId { get; private set; }
+        public string Name { get; private set; }
+        public string Major { get; private set; }
+        public int MaxCredits { get; private set; }
+
+        public List<string> CompletedCourses { get; private set; }
+        public List<Course> RegisteredCourses { get; private set; }
+
+        public Student(string id, string name, string major, int maxCredits = 18, List<string> completedCourses = null)
+        {
+            StudentId = id;
+            Name = name;
+            Major = major;
+            MaxCredits = maxCredits;
+            CompletedCourses = completedCourses ?? new List<string>();
+            RegisteredCourses = new List<Course>();
+        }
+
+        public int GetTotalCredits()
+        {
+            int sum = 0;
+            foreach (var course in RegisteredCourses)
+            {
+                sum += course.Credits;
+            }
+            return sum; 
+        }
+
+        public bool CanAddCourse(Course course)
+        {
+            // TODO:
+            // 1. Course should not already be registered
+            if (RegisteredCourses.Contains(course))
+            { return false; }
+            // 2. Total credits + course credits <= MaxCredits
+            if(course.Credits+GetTotalCredits()<=MaxCredits)
+            {
+                foreach(var pre in course.Prerequisites)
+                {
+                    if(!CompletedCourses.Contains(pre))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool AddCourse(Course course)
+        {
+            // TODO:
+            // 1. Call CanAddCourse
+            if (CanAddCourse(course) == true)
+            {
+                if(!course.IsFull())
+                {
+                    RegisteredCourses.Add(course);
+                    course.EnrollStudent();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool DropCourse(string courseCode)
+        {
+            Course course = RegisteredCourses.Find(x => x.CourseCode == courseCode);
+            if(course==null)
+            {
+                return false;
+            }
+            RegisteredCourses.Remove(course);
+            course.DropStudent();
+            return true;
+        }
+
+        public void DisplaySchedule()
+        {
+            if(RegisteredCourses.Count==0)
+            {
+                Console.WriteLine("There is no course registered");
+                return;
+            }
+            foreach(var i in RegisteredCourses)
+            {
+                Console.WriteLine($"{i.CourseCode},{i.CourseName}, {i.Credits} ");
+            }
+        }
+    }
+}
---
+++ UniverSity Course Registration System/UniverSity Course Registration System/UniversitySystem.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University_Course_Registration_System
+{
+    // =========================
+    // University System Class
+    // =========================
+    public class UniversitySystem
+    {
+        public Dictionary<string, Course> AvailableCourses { get; private set; }
+        public Dictionary<string, Student> Students { get; private set; }
+
+        public UniversitySystem()
+        {
+            AvailableCourses = new Dictionary<string, Course>();
+            Students = new Dictionary<string, Student>();
+        }
+
+        public void AddCourse(string code, string name, int credits, int maxCapacity = 50, List<string> prerequisites = null)
+        {
+            // TODO:
+            // 1. Throw ArgumentException if course code exists
+            if (AvailableCourses.ContainsKey(code))
+            {
+                throw new ArgumentException();
+            }
+            Course course = new Course(code, name, credits, maxCapacity, prerequisites);
+
+            // 2. Create Course object
+            // 3. Add to AvailableCourses
+            AvailableCourses.Add(code, course);
+        }
+
+        public void AddStudent(string id, string name, string major, int maxCredits = 18, List<string> completedCourses = null)
+        {
+            // TODO:
+            // 1. Throw ArgumentException if student ID exists
+            if (Students.ContainsKey(id))
+            {
+                throw new ArgumentException();
+            }
+            // 2. Create Student object
+            Student student = new Student(id,name, major, maxCredits, completedCourses);
+            Students.Add(id,student);
+            // 3. Add to Students dictionary
+            throw new NotImplementedException();
+        }
+
+        public bool RegisterStudentForCourse(string studentId, string courseCode)
+        {
+            // TODO:
+            // 1. Validate student and course existence
+             if(Students.ContainsKey(studentId)&&AvailableCourses.ContainsKey(courseCode))
+            {
+                Student student = Students[studentId];
+                Course course = AvailableCourses[courseCode];
+                student.AddCourse(course);
+                return true;
+            }
+            return false;
+        }
+
+        public bool DropStudentFromCourse(string studentId, string courseCode)
+        {
+            if (Students.ContainsKey(studentId) && AvailableCourses.ContainsKey(courseCode))
+            {
+                Student student = Students[studentId];
+                Course course = AvailableCourses[courseCode];
+                student.DropCourse(courseCode);
+                return true;
+            }
+            return false;
+        }
+
+        public void DisplayAllCourses()
+        {
+            // TODO:
+            foreach(var i in AvailableCourses)
+            {
+                Course course = i.Value;
+                Console.WriteLine($"{course.CourseCode}, {course.CourseName}, {course.Credits},{course.GetEnrollmentInfo}");
+            }
+            // Display course code, name, credits, enrollment info\
+        }
+
+        public void DisplayStudentSchedule(string studentId)
+        {
+            // TODO:
+            // Validate student existence
+            if(Students.ContainsKey(studentId))
+            {
+                Student student = Students[studentId];
+                student.DisplaySchedule();
+            }
+            // Call student.DisplaySchedule()
+        }
+
+        public void DisplaySystemSummary()
+        {
+            // TODO:
+            // Display total students, total courses, average enrollment
+            Console.WriteLine($"{Students.Count}, {AvailableCourses.Count}");
+        }
+    }
+}
