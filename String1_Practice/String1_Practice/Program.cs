f20908b1aafa630ac0e0d7be58e701b2e78d53c1
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-02-12 00:38:47 +0530

    Add files via upload

---
+++ String1_Practice/String1_Practice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace String1_Practice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the sentence");
+            string word = Console.ReadLine();
+            string[] words = word.Split(" ");
+            string ans = "";
+                for(int i=words.Length-1; i>=0; i--)
+                {
+                    ans += words[i];
+                    if(i!=0)
+                    {
+                        ans += " ";
+                    }
+                }
+            if(words.Length%2!=0)
+            {
+                string rev = new string(ans.Reverse().ToArray());
+                Console.WriteLine(rev);
+                return;
+            }
+            Console.WriteLine(ans);
+            
+        }
+    }
+}
