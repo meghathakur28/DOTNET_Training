50d4618691f8e79fda807ffcb1be29815dcaa8f6
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-02-12 01:39:15 +0530

    Add files via upload

---
+++ String2_Practice/String2_Practice/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace String2_Practice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the username");
+            string word = Console.ReadLine();
+
+            if (word.Length != 8)
+            {
+                Console.WriteLine("Invalid username");
+                return;
+            }
+            else
+            {
+                string last = word.Substring(word.Length - 3, 3);
+                string first = word.Substring(0, 5);
+                bool upper4 = Regex.IsMatch(first, @"[A-Z]{4}[@]{1}");
+                if (upper4)
+                {
+                    int lasts;
+                    if (int.TryParse(last, out lasts))
+                    {
+                        if (lasts >= 101 && lasts <= 115)
+                        {
+                            string pass = word.Substring(0, 4);
+                            int sum = 0;
+                            for(int i=0;i<4;i++)
+                            {
+                                sum += (int)pass[i];
+                            }
+                            Console.WriteLine("Password: TECH"+ "_" + sum);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid");
+                    return;
+                }
+            }
+
+        }
+    }
+}
