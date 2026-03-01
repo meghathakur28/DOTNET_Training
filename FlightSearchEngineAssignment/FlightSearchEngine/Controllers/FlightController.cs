a986714f61bba929361def0daa927ad655ac1f29
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-03-02 02:19:55 +0530

    Add files via upload

---
+++ FlightSearchEngineAssignment/FlightSearchEngine/Controllers/FlightController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FlightSearchEngine.Data;
+using FlightSearchEngine.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FlightSearchEngine.Controllers
+{
+    public class FlightController : Controller
+    {
+        private readonly DatabaseHelper _db;
+
+        public FlightController(IConfiguration configuration)
+        {
+            _db = new DatabaseHelper(configuration);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = new SearchViewModel();
+            var sources = await _db.GetSourcesAsync();
+            var destinations = await _db.GetDestinationsAsync();
+            model.SourceList = new SelectList(sources);
+            model.DestinationList = new SelectList(destinations);
+            return View(model);
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> SearchFlights(SearchViewModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        var sources = await _db.GetSourcesAsync();
+        //        var destinations = await _db.GetDestinationsAsync();
+        //        model.SourceList = new SelectList(sources);
+        //        model.DestinationList = new SelectList(destinations);
+        //        return View("Index", model);
+        //    }
+
+        //    var results = await _db.SearchFlightsAsync(model.Source, model.Destination, model.NumberOfPersons);
+        //    ViewBag.SearchType = "Flight";
+        //    return View("Results", results);
+        //}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SearchFlightsWithHotels(SearchViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var sources = await _db.GetSourcesAsync();
+                var destinations = await _db.GetDestinationsAsync();
+                model.SourceList = new SelectList(sources);
+                model.DestinationList = new SelectList(destinations);
+                return View("Index", model);
+            }
+
+            var results = await _db.SearchFlightsWithHotelsAsync(model.Source, model.Destination, model.NumberOfPersons);
+            ViewBag.SearchType = "FlightHotel";
+            return View("Results", results);
+        }
+        public async Task<IActionResult> SearchFlights(SearchViewModel model)
+        {
+                var sources = await _db.GetSourcesAsync();
+                var destinations = await _db.GetDestinationsAsync();
+                model.SourceList = new SelectList(sources);
+                model.DestinationList = new SelectList(destinations);
+                return View("Index", model);
+           
+
+            var results = await _db.SearchFlightsAsync(model.Source, model.Destination, model.NumberOfPersons);
+            ViewBag.SearchType = "Flight";
+            return View("Results", results);
+        }
+    }
+}
