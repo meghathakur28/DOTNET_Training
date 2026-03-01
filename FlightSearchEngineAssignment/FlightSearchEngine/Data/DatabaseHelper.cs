d07372c0c4943d4a0473b8902e4e19b8c6be2141
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-03-02 02:20:50 +0530

    Add files via upload

---
+++ FlightSearchEngineAssignment/FlightSearchEngine/Data/DatabaseHelper.cs
@@ -0,0 +1,126 @@
+﻿using FlightSearchEngine.Models;
+using Microsoft.Data.SqlClient;
+//using System.Data.SqlClient;
+
+namespace FlightSearchEngine.Data
+{
+    public class DatabaseHelper
+    {
+        private readonly string _connectionString;
+
+        public DatabaseHelper(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<List<string>> GetSourcesAsync()
+        {
+            var sources = new List<string>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                using (var command = new SqlCommand("sp_GetSources", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    await connection.OpenAsync();
+                    var reader = await command.ExecuteReaderAsync();
+                    while (await reader.ReadAsync())
+                    {
+                        sources.Add(reader["Source"].ToString());
+                    }
+                }
+            }
+
+            return sources;
+        }
+
+        public async Task<List<string>> GetDestinationsAsync()
+        {
+            var destinations = new List<string>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                using (var command = new SqlCommand("sp_GetDestinations", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    await connection.OpenAsync();
+                    var reader = await command.ExecuteReaderAsync();
+                    while (await reader.ReadAsync())
+                    {
+                        destinations.Add(reader["Destination"].ToString());
+                    }
+                }
+            }
+
+            return destinations;
+        }
+
+        public async Task<List<FlightResult>> SearchFlightsAsync(string source, string destination, int persons)
+        {
+            var results = new List<FlightResult>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                using (var command = new SqlCommand("sp_SearchFlights", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Source", source);
+                    command.Parameters.AddWithValue("@Destination", destination);
+                    command.Parameters.AddWithValue("@Persons", persons);
+
+                    await connection.OpenAsync();
+                    var reader = await command.ExecuteReaderAsync();
+                    while (await reader.ReadAsync())
+                    {
+                        var flight = new FlightResult
+                        {
+                            FlightId = Convert.ToInt32(reader["FlightId"]),
+                            FlightName = reader["FlightName"].ToString(),
+                            FlightType = reader["FlightType"].ToString(),
+                            Source = reader["Source"].ToString(),
+                            Destination = reader["Destination"].ToString(),
+                            TotalCost = Convert.ToDecimal(reader["TotalCost"])
+                        };
+                        results.Add(flight);
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        public async Task<List<FlightHotelResult>> SearchFlightsWithHotelsAsync(string source, string destination, int persons)
+        {
+            var results = new List<FlightHotelResult>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                using (var command = new SqlCommand("sp_SearchFlightsWithHotels", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Source", source);
+                    command.Parameters.AddWithValue("@Destination", destination);
+                    command.Parameters.AddWithValue("@Persons", persons);
+
+                    await connection.OpenAsync();
+                    var reader = await command.ExecuteReaderAsync();
+                    while (await reader.ReadAsync())
+                    {
+                        var item = new FlightHotelResult
+                        {
+                            FlightId = Convert.ToInt32(reader["FlightId"]),
+                            FlightName = reader["FlightName"].ToString(),
+                            Source = reader["Source"].ToString(),
+                            Destination = reader["Destination"].ToString(),
+                            HotelName = reader["HotelName"].ToString(),
+                            TotalCost = Convert.ToDecimal(reader["TotalCost"])
+                        };
+                        results.Add(item);
+                    }
+                }
+            }
+
+            return results;
+        }
+    }
+}
---
+++ FlightSearchEngineAssignment/FlightSearchEngine/Models/SearchViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FlightSearchEngine.Models
+{
+    public class SearchViewModel
+    {
+        [Required(ErrorMessage = "Please select a source city")]
+        public string Source { get; set; }
+
+        [Required(ErrorMessage = "Please select a destination city")]
+        public string Destination { get; set; }
+
+        [Required(ErrorMessage = "Please enter number of persons")]
+        [Range(1, 10, ErrorMessage = "Number of persons must be between 1 and 10")]
+        public int NumberOfPersons { get; set; }
+
+        public SelectList SourceList { get; set; }
+        public SelectList DestinationList { get; set; }
+    }
+}
